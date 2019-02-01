5036206b9032dbf32028a09fc5d45cff6cc562ad
Vladimir Bozhinovski <bozhinovski.vladimir@gmail.com>
2019-02-01 16:54:47 +0100

    initial commit

---
+++ src/app/containers/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { User } from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Store } from 'store';
+import { AuthService } from './../../../auth/shared/services/auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+	selector: 'app-root',
+	styleUrls: ['app.component.scss'],
+	template: `
+		<div>
+			<app-header [user]="user$ | async" (logout)="onLogout()"> </app-header>
+			<app-nav *ngIf="(user$ | async)?.isAuthenticated"></app-nav>
+			<div class="wrapper">
+				<router-outlet></router-outlet>
+			</div>
+		</div>
+	`,
+})
+export class AppComponent implements OnInit, OnDestroy {
+	user$: Observable<User>;
+	subscription: Subscription;
+
+	constructor(private store: Store, private authService: AuthService, private router: Router) {}
+
+	ngOnInit() {
+		this.subscription = this.authService.auth$.subscribe();
+		this.user$ = this.store.select<User>('user');
+	}
+
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
+	}
+
+	async onLogout() {
+		await this.authService.logoutUser();
+		this.router.navigate(['/auth/login'])
+	}
+}
